3ded6c02d7a044f7d3bd8d979dd67ce55beb6c9d
Uedsonoliveira <uedson.oliveira@gmail.com>
2024-09-25 17:17:17 -0300

    Removendo a pasta 06_signo_do_dia_modulos e seus arquivos

--- 06_signo_do_dia_modulos/main.js
+++
@@ -1,8 +0,0 @@
-import colecao_signos from './dados/dados.js';
-import retorna_signo from './funcoes/funcoes.js';
-
-let dataApp = new Date ();
-
-const nome_signo = retorna_signo(colecao_signos, dataApp);
-
-console.log("\nO signo do dia é: " + nome_signo +"\n");
